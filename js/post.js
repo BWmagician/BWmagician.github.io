b9fc793e6cea54f85318a44bc8e3a5965240a310
BWmagician <master10032spark@gmail.com>
2025-07-08 16:06:53 +0800

    Site updated: 2025-07-08 16:06:49

--- js/post.js
+++ js/post.js
@@ -1,16 +1,67 @@
 // 在aside.js之后
 let postContent = document.getElementsByTagName("postContent");
 
-//没有二级标题就把文章内容覆盖掉右侧边栏
-if (asideHeadings.length == 0) {
-    postContent[0].setAttribute('style', 'margin-right:20px');
-    aside[0].setAttribute('style', 'z-index:-1;')
-}
-
-//给代码块的列号对齐
+//给代码块的列号对齐，没用
 let gutterLine = document.querySelectorAll(".gutter .line");
 let codeLine = document.querySelectorAll(".code .line");
-for (let i = 0; i < gutterLine.length; ++i) {
-    gutterLine[i].setAttribute('style', `height:${codeLine[i].offsetHeight}px`);
+let code = document.querySelectorAll(".highlight .code");
+
+let resizeObserverCode = new Array(code.length);
+
+// for(let i = 0; i < code.length; ++i) {
+//     resizeObserverCode[i] = new ResizeObserver(x => {
+//         for (let i = 0; i < gutterLine.length; ++i) {
+//             gutterLine[i].setAttribute('style', `height:${codeLine[i].offsetHeight}px`);
+//             console.log(gutterLine[i],codeLine[i],codeLine[i].offsetTop);
+//         }
+//     });
+
+//     resizeObserverCode[i].observe(code[i]);
+// }
+
+let codePosition = new Array(code.length);
+let codeLineSet = new Array(code.length); 
+
+for (let i = 0; i < code.length; ++i) {
+    codePosition[i] = code[i].getBoundingClientRect().top + window.scrollY;
+    codeLineSet[i] = [];
 }
-console.log(gutterLine);
+
+for (let i = 0, j = 0; i < codeLine.length; ++i) {
+    let codeLinePos = codeLine[i].getBoundingClientRect().top + window.scrollY;
+    while(j + 1 < code.length && codeLinePos >= codePosition[j + 1]) ++j;
+    codeLineSet[j].push(i);
+    // console.log(i,j,codeLineSet[j][codeLineSet[j].length-1]);
+    // console.log(codeLinePos, codePosition[j]);
+}
+
+let codeLinePosition = new Array(codeLine.length);
+
+for(let i = 0; i < code.length; ++i) {
+    resizeObserverCode[i] = new ResizeObserver(x => { 
+        // console.log(i);
+        for (let j = 0; j < codeLineSet[i].length; ++j) {
+            let k = codeLineSet[i][j];
+            codeLinePosition[k] = codeLine[k].getBoundingClientRect().top; 
+            // console.log("codeLinePosition:",codeLinePosition[k]);
+        }
+        for (let j = 0; j < codeLineSet[i].length; ++j) {
+            let k = codeLineSet[i][j], realHeight = 0;
+            // console.log(j,k);
+            if(j == 0){
+                realHeight = codeLinePosition[k+1] - code[i].getBoundingClientRect().top - 3;
+            }
+            else if(j == codeLineSet[i].length - 1){
+                realHeight = 0;
+            }
+            else{
+                realHeight
+                = codeLinePosition[k+1] - codeLinePosition[k];
+            }
+            // console.log(`k:${k},realHeight:${realHeight}`);
+            gutterLine[k].setAttribute('style', `height:${realHeight}px`);
+        }
+    });
+
+    resizeObserverCode[i].observe(code[i]);
+}
